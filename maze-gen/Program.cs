30bb9af330012eadaf10789cd58bc0437c59a4ca
hamilt79@miamioh.edu <hamilt79@miamioh.edu>
2025-11-05 21:26:45 -0500

    Some comments, and added more elements of the maze

--- maze-gen/Program.cs
+++ maze-gen/Program.cs
@@ -17,45 +17,87 @@ namespace MazeFromExcel
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length != 2)
             {
                 Console.Error.WriteLine("Bad arguments o7");
                 return;
             }
             string filePath = args[0];
+            int sheetNum;
+            try
+            {
+                sheetNum = int.Parse(args[1]);
+            }
+            catch (Exception)
+            {
+                Console.Error.WriteLine("Bad sheet number");
+                return;
+            }
 
             ExcelPackage.License.SetNonCommercialPersonal("Miami Student");
             using (var package = new ExcelPackage(new FileInfo(filePath)))
             {
-                var ws = package.Workbook.Worksheets[0];
-                
+                if (package.Workbook.Worksheets.Count <= sheetNum || sheetNum < 0)
+                {
+                    Console.Error.WriteLine("Bad sheet number");
+                    return;
+                }
+                var ws = package.Workbook.Worksheets[sheetNum];
+
                 int maxRow = ws.Dimension.End.Row;
                 int maxCol = ws.Dimension.End.Column;
                 Console.WriteLine($"Max Row: {maxRow}");
                 Console.WriteLine($"Max Col: {maxCol}");
-                var openCells = new HashSet<(int, int)>();
 
+                var moveableCells = new HashSet<(int, int)>();
+                var doors = new HashSet<(int, int)>();
+                var walls = new HashSet<(int, int)>();
+                var freezeTraps = new HashSet<(int, int)>();
+                var levers = new HashSet<(int, int)>();
+
+
+                // If I wanted to start my index at 1 I would have used lua....
+                // Sadly x sheets start at 1.
                 for (int row = 1; row <= maxRow; row++)
                 {
                     for (int col = 1; col <= maxCol; col++)
                     {
                         var cell = ws.Cells[row, col];
                         var fill = cell.Style.Fill.BackgroundColor;
+                        var text = cell.Text;
 
-                        bool isWhite = fill.Rgb == null || fill.Rgb.ToUpper() == "FFFFFFFF";
-                        bool isBlack = fill.Rgb != null && fill.Rgb.ToUpper() == "FF000000";
+                        // Do not believe this is needed based on docs.
+                        // Here just in case
+                        if (fill.Rgb == null)
+                        {
+                            Console.Error.WriteLine("Empty Square Found. \nEnsure all grid elements are valid.");
+                            return;
+                        }
 
-                        int x = col;
-                        int y = row;
+                        // ARGB format.....ew
+                        bool isWall = fill.Rgb.ToUpper() == "FF000000"; // black
+                        bool isFreezeTrap = fill.Rgb.ToUpper() == "FF0000FF"; // blue
+                        bool isDoor = fill.Rgb.ToUpper() == "FF5A0E00"; // brown
+                        bool isLever = fill.Rgb.ToUpper() == "FFFF00FF"; // magenta
 
-                        if (isWhite && !isBlack)
-                            openCells.Add((x, y));
+                        bool isMoveable = fill.Rgb.ToUpper() == "FFFFFFFF" || isFreezeTrap || isDoor || isLever; // white
+
+                        if (isMoveable)
+                            moveableCells.Add((col, row));
+                        if (isLever)
+                            levers.Add((col, row));
+                        if (isDoor)
+                            doors.Add((col, row));
+                        if (isWall)
+                            walls.Add((col, row));
+                        if (isFreezeTrap)
+                            freezeTraps.Add((col, row));
                     }
                 }
 
                 var adjacents = new List<(int, int, int, int)>();
 
-                foreach (var (x, y) in openCells)
+                foreach (var (x, y) in moveableCells)
                 {
                     var neighbors = new (int, int)[]
                     {
@@ -64,18 +106,34 @@ namespace MazeFromExcel
 
                     foreach (var (nx, ny) in neighbors)
                     {
-                        if (openCells.Contains((nx, ny)))
+                        if (moveableCells.Contains((nx, ny)))
                             adjacents.Add((x, y, nx, ny));
                     }
                 }
 
-                Console.WriteLine("Cells:");
-                foreach (var (x, y) in openCells)
+                Console.WriteLine("% Cells:");
+                foreach (var (x, y) in moveableCells)
                     Console.WriteLine($"cell({x},{y}).");
 
-                Console.WriteLine("\nAdjacents:");
+                Console.WriteLine("\n% Adjacents:");
                 foreach (var (x1, y1, x2, y2) in adjacents)
                     Console.WriteLine($"adjacent(({x1},{y1}),({x2},{y2})).");
+
+                Console.WriteLine("\n% Freeze Traps:");
+                foreach (var (x, y) in freezeTraps)
+                    Console.WriteLine($"freeze_trap({x},{y}).");
+
+                Console.WriteLine("\n% Doors:");
+                foreach (var (x, y) in freezeTraps)
+                    Console.WriteLine($"door({x},{y}).");
+
+                Console.WriteLine("\n% Walls:");
+                foreach (var (x, y) in walls)
+                    Console.WriteLine($"wall({x},{y}).");
+
+                Console.WriteLine("\n% Levers:");
+                foreach (var (x, y) in walls)
+                    Console.WriteLine($"lever({x},{y}).");
             }
         }
     }
